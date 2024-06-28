a75e0282d7fc8e0468de3c1d4693f51f75a5463b
SnakouZ <gillianlevert.dev@protonmail.com>
2024-06-28 20:49:21 +0200

    first push

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,48 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-geist-mono)"],
-      },
+  
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        "brown-light": {
+          extend: "dark", // <- inherit default values from dark theme
+          colors: {
+            background: "#0D001A",
+            foreground: "#ffffff",
+            primary: {
+              50: "#ffffff",
+              100: "#FBF5EC",
+              200: "#FBF5EC",
+              300: "#F4EADE",
+              400: "#EADCD0",
+              500: "#DDCABC",
+              600: "#BE9C89",
+              700: "#9F725E",
+              800: "#804D3B",
+              900: "#6A3124",
+              DEFAULT: "#DDCABC",
+              foreground: "#ffffff",
+            },
+            focus: "#F182F6",
+          },
+          layout: {
+            disabledOpacity: "0.3",
+            radius: {
+              small: "4px",
+              medium: "6px",
+              large: "8px",
+            },
+            borderWidth: {
+              small: "1px",
+              medium: "2px",
+              large: "3px",
+            },
+          },
+        },
+      },
+    }),
+  ],
 }
