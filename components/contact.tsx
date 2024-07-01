9e6afdf8c3c9a3494c62a3a78ef4721039c671c4
SnakouZ <gillianlevert.dev@protonmail.com>
2024-07-01 17:56:13 +0200

    prettier

--- components/contact.tsx
+++ components/contact.tsx
@@ -35,7 +35,7 @@ export const Contact = () => {
       }
       resultTimeout.current = setTimeout(clearResult, 5000);
     },
-    [clearResult]
+    [clearResult],
   );
 
   const onSubmit = useCallback(
@@ -44,7 +44,7 @@ export const Contact = () => {
 
       if (!isRgpdChecked) {
         setTemporaryResult(
-          "Veuillez accepter la politique RGPD avant d'envoyer le formulaire."
+          "Veuillez accepter la politique RGPD avant d'envoyer le formulaire.",
         );
 
         return;
@@ -55,8 +55,8 @@ export const Contact = () => {
       if (now - lastSubmitTime.current < cooldownPeriod) {
         setTemporaryResult(
           `Veuillez attendre ${Math.ceil(
-            (cooldownPeriod - (now - lastSubmitTime.current)) / 1000
-          )} secondes avant de renvoyer un message.`
+            (cooldownPeriod - (now - lastSubmitTime.current)) / 1000,
+          )} secondes avant de renvoyer un message.`,
         );
 
         return;
@@ -72,7 +72,7 @@ export const Contact = () => {
 
       formData.append(
         "access_key",
-        process.env.NEXT_PUBLIC_MAILER_KEY as string
+        process.env.NEXT_PUBLIC_MAILER_KEY as string,
       );
 
       try {
@@ -96,13 +96,13 @@ export const Contact = () => {
       } catch (error) {
         console.error("Erreur lors de l'envoi du formulaire:", error);
         setTemporaryResult(
-          "Une erreur s'est produite lors de l'envoi du formulaire."
+          "Une erreur s'est produite lors de l'envoi du formulaire.",
         );
       } finally {
         setIsSubmitting(false);
       }
     },
-    [isSubmitting, setTemporaryResult, isRgpdChecked]
+    [isSubmitting, setTemporaryResult, isRgpdChecked],
   );
 
   const handleRgpdChange = (checked: boolean) => {
